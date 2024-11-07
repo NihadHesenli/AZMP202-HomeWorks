73ba96600bd5f6c481742485b1c269a22443ebbb
NihadHesenli <niadfloppa@gmail.com>
2024-11-07 12:43:06 +0400

    catdira bilmedim

---
+++ 5.JS-class/index.js
@@ -0,0 +1,18 @@
+// task 1
+
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     calculatorArea(){
+//         return this.width*this.height;
+//     }
+//     calculaterPerimeter(){
+//         return 2 * (this.width + this.height);
+//     }
+// }
+// let rect = new Rectangle(5,10)
+// console.log("Area of the rectangle:",rect.calculatorArea());
+// console.log("Perimeter of the rectangle:",rect.calculaterPerimeter());
